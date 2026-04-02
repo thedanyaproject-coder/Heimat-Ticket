5dcc72cc075f7e479b5ef55b7ed1aac2caccd43d
Gasthaus Alt Grieth <270933862+thedanyaproject-coder@users.noreply.github.com>
2026-04-02 20:48:55 +0200

    Update reserve.js

--- API/reserve.js
+++ API/reserve.js
@@ -24,4 +24,6 @@ export default async function handler(req, res) {
 
     res.status(200).json(data);
   } catch (e) {
-    res.status(500).json({
+    res.status(500).json({ success: false, message: "Reserve proxy error" });
+  }
+}
