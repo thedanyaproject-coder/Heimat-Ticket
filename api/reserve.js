501669916e706c9736b411b806bb3ccbee5567f2
Gasthaus Alt Grieth <270933862+thedanyaproject-coder@users.noreply.github.com>
2026-04-02 22:51:54 +0200

    Update reserve.js

--- api/reserve.js
+++ api/reserve.js
@@ -8,7 +8,7 @@ module.exports = async function (req, res) {
     const food = req.query.food || "Nein";
 
     const url =
-      "https://script.google.com/macros/s/AKfycbzURyof90tecaKRlR-iqmLwnDpq69xzT" +
+      "https://script.google.com/macros/s/AKfycbzURyof90tecaKRlR-iqmLwnDpq69xzT5L9Te7OKGsp35EcY1VtXWidLz092f6QLkd64g/exec" +
       "?action=reserve" +
       "&first_name=" + encodeURIComponent(first_name) +
       "&last_name=" + encodeURIComponent(last_name) +
@@ -22,7 +22,6 @@ module.exports = async function (req, res) {
 
     res.setHeader("Content-Type", "application/json; charset=utf-8");
     res.status(200).send(text);
-
   } catch (error) {
     res.status(500).json({
       success: false,
