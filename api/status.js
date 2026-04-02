3bf1367697103f4f68d62254e6bef7ceef204b71
Gasthaus Alt Grieth <270933862+thedanyaproject-coder@users.noreply.github.com>
2026-04-02 21:16:49 +0200

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,1 +1,18 @@
+module.exports = async function (req, res) {
+  try {
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbzURyof90tecaKRlR-iqmLwnDpq69xzT5L9Te7OKGsp35EcY1VtXWidLz092f6QLkd64g/exec?action=status"
+    );
 
+    const text = await response.text();
+
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    res.status(200).send(text);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Status proxy error",
+      error: String(error)
+    });
+  }
+};
