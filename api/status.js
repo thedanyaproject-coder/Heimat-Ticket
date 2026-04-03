eb7ad519db1d6de16e1a8fe59879cc62c56363fe
Gasthaus Alt Grieth <270933862+thedanyaproject-coder@users.noreply.github.com>
2026-04-03 15:07:58 +0200

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,23 +1,9 @@
 module.exports = async function (req, res) {
   try {
-    const first_name = req.query.first_name || "";
-    const last_name = req.query.last_name || "";
-    const email = req.query.email || "";
-    const phone = req.query.phone || "";
-    const persons = req.query.persons || "";
-    const food = req.query.food || "Nein";
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbzacbdB_CkW9qqq5-ABFB_5g8vatoJLsoN9MkgYCyFD--YYkxQGFSkuoZz_GZ8eyKQnZw/exec?action=status"
+    );
 
-    const url =
-      "https://script.google.com/macros/s/AKfycbzacbdB_CkW9qqq5-ABFB_5g8vatoJLsoN9MkgYCyFD--YYkxQGFSkuoZz_GZ8eyKQnZw/exec" +
-      "?action=reserve" +
-      "&first_name=" + encodeURIComponent(first_name) +
-      "&last_name=" + encodeURIComponent(last_name) +
-      "&email=" + encodeURIComponent(email) +
-      "&phone=" + encodeURIComponent(phone) +
-      "&persons=" + encodeURIComponent(persons) +
-      "&food=" + encodeURIComponent(food);
-
-    const response = await fetch(url);
     const text = await response.text();
 
     res.setHeader("Content-Type", "application/json; charset=utf-8");
@@ -25,7 +11,7 @@ module.exports = async function (req, res) {
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: "Reserve proxy error",
+      message: "Status proxy error",
       error: String(error)
     });
   }
