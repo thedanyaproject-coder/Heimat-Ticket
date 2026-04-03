c825286cebfcfbb21086cc64c7148d0bf8095540
Gasthaus Alt Grieth <270933862+thedanyaproject-coder@users.noreply.github.com>
2026-04-03 14:40:58 +0200

    Update lookup.js

--- api/lookup.js
+++ api/lookup.js
@@ -1,1 +1,22 @@
+module.exports = async function (req, res) {
+  try {
+    const ticket = req.query.ticket || "";
 
+    const url =
+      "https://script.google.com/macros/s/AKfycbw4rZ7BqTOVG3vAayLDA7ipDs1VUX-hYxnukwCAgKQn9q6SpW940pSxm6s3YJ0KBGiErw/exec" +
+      "?action=lookup" +
+      "&ticket=" + encodeURIComponent(ticket);
+
+    const response = await fetch(url);
+    const text = await response.text();
+
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    res.status(200).send(text);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Lookup proxy error",
+      error: String(error)
+    });
+  }
+};
