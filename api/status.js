f4ce62b7bf5f1a63402766f12570fd4c5e6b1ad9
Gasthaus Alt Grieth <270933862+thedanyaproject-coder@users.noreply.github.com>
2026-04-03 10:32:33 +0200

    Update status.js

--- api/status.js
+++ api/status.js
@@ -3,9 +3,7 @@ module.exports = async function (req, res) {
     const response = await fetch(
       "https://script.google.com/macros/s/AKfycbyZPrncuIUZJKODeYeaOhKj8A1fEN7rNzEKS8DA7LJRZgg8V1mMP2EIL63dZStQCDumPA/exec?action=status"
     );
-
     const text = await response.text();
-
     res.setHeader("Content-Type", "application/json; charset=utf-8");
     res.status(200).send(text);
   } catch (error) {
